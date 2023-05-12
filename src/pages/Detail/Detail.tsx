a36d8a9bd6dae01ffc1fe97aea2c73eaefee4688
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-05-12 17:24:03 +0900

    Initial commit

---
+++ src/pages/Detail/Detail.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Main() {
+  return <div />;
+}
+export default Main;
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,13 @@
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
